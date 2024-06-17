fdaf73570a715ddaba4e38699426b26f6847e54e
Caroline <cave0001@stud.ked.dk>
2024-06-17 11:59:21 +0200

    stavefejl

--- src/app/components/bookingflow/Billingform.jsx
+++ src/app/components/bookingflow/Billingform.jsx
@@ -7,13 +7,8 @@ import CountrySelector from "./CountrySelector";
 function Billingform({ setPage, setBookingId }) {
   const checkForError = (e) => {
     if (e.target.name == "expodate") {
-      if (e.target.value.toString().length === 2)
-        e.target.value = e.target.value + "/";
-      else if (
-        e.target.value.toString().length === 3 &&
-        e.target.value.toString().charAt(2) === "/"
-      )
-        e.target.value = e.target.value.replace("/", "");
+      if (e.target.value.toString().length === 2) e.target.value = e.target.value + "/";
+      else if (e.target.value.toString().length === 3 && e.target.value.toString().charAt(2) === "/") e.target.value = e.target.value.replace("/", "");
     }
   };
 
@@ -38,109 +33,39 @@ function Billingform({ setPage, setBookingId }) {
     <div>
       <form className={styles} onSubmit={formHandler}>
         <fieldset>
-          <legend className={styles.bookingSubtitle}>
-            Billing Information
-          </legend>
-          <p className={styles.hint}>
-            Please fill out the information for the desired billing address
-          </p>
+          <legend className={styles.bookingSubtitle}>Billing Information</legend>
+          <p className={styles.hint}>Please fill out the information for the desired billing address</p>
           <div className={styles.inputBox}>
             <label htmlFor="fullname">Full name</label>
-            <p className={styles.hint}>
-              Write you full name including first, middle, and lastname
-            </p>
-            <input
-              className={styles.inputField}
-              name="fullname"
-              id="fullname"
-              type="text"
-              placeholder="Jane Doe"
-              pattern="[A-Za-zæøåÆØÅ]{2,}\s?)+"
-              autoCapitalize="on"
-              autoCorrect="off"
-              title="Please fill out full name"
-              required
-            />
+            <p className={styles.hint}>Write your full name including first, middle, and lastname</p>
+            <input className={styles.inputField} name="fullname" id="fullname" type="text" placeholder="Jane Doe" pattern="[A-Za-zæøåÆØÅ]{2,}\s?)+" autoCapitalize="on" autoCorrect="off" title="Please fill out full name" required />
           </div>
 
           <div className={styles.inputBox}>
             <label htmlFor="email">Email</label>
 
-            <input
-              className={styles.inputField}
-              name="email"
-              id="email"
-              type="email"
-              placeholder="jane@mail.com"
-              pattern="[^@\s]+@[^@\s]+\.[^@\s]+"
-              title="Invalid email address"
-              autoCapitalize="off"
-              autoCorrect="off"
-            />
+            <input className={styles.inputField} name="email" id="email" type="email" placeholder="jane@mail.com" pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Invalid email address" autoCapitalize="off" autoCorrect="off" />
 
             <div className={styles.inputBox}>
               <label htmlFor="phone">Phone</label>
-              <p className={styles.hint}>
-                Must be 8 characters end exclude country-code
-              </p>
-              <input
-                id="phone"
-                type="tel"
-                name="phone"
-                required
-                inputMode="numeric"
-                className={`${styles.inputField} ${styles.inputTel}`}
-                placeholder="20202020"
-                title="Invalid phone number"
-                pattern="^(?:\d{2}\s?\d{2}\s?\d{2}\s?\d{2}|\d{8})$"
-                maxLength={8}
-              />
+              <p className={styles.hint}>Must be 8 characters end exclude country-code</p>
+              <input id="phone" type="tel" name="phone" required inputMode="numeric" className={`${styles.inputField} ${styles.inputTel}`} placeholder="20202020" title="Invalid phone number" pattern="^(?:\d{2}\s?\d{2}\s?\d{2}\s?\d{2}|\d{8})$" maxLength={8} />
             </div>
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="address">Address</label>
-            <p className={styles.hint}>
-              Fill out full address including number and/or floor
-            </p>
-            <input
-              className={styles.inputField}
-              name="address"
-              id="address"
-              type="text"
-              placeholder="Streetname 1 st.tv"
-              title="Invalid address"
-              autoCapitalize="on"
-              required
-            />
+            <p className={styles.hint}>Fill out full address including number and/or floor</p>
+            <input className={styles.inputField} name="address" id="address" type="text" placeholder="Streetname 1 st.tv" title="Invalid address" autoCapitalize="on" required />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="zip">Zip</label>
 
-            <input
-              className={`${styles.inputField} ${styles.inputZip}`}
-              name="zip"
-              id="zip"
-              placeholder="2000"
-              title="Invalid zip"
-              pattern="[+0-9]{4,}"
-              required
-              inputmode="numeric"
-            />
+            <input className={`${styles.inputField} ${styles.inputZip}`} name="zip" id="zip" placeholder="2000" title="Invalid zip" pattern="[+0-9]{4,}" required inputmode="numeric" />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="city">City</label>
 
-            <input
-              className={styles.inputField}
-              name="city"
-              id="city"
-              type="text"
-              title="Invalid city"
-              placeholder="Copenhagen"
-              autoCapitalize="on"
-              autoCorrect="on"
-              required
-            />
+            <input className={styles.inputField} name="city" id="city" type="text" title="Invalid city" placeholder="Copenhagen" autoCapitalize="on" autoCorrect="on" required />
           </div>
           {/* <div className={styles.inputBox}>
             <label htmlFor="country">County</label>
@@ -154,69 +79,24 @@ function Billingform({ setPage, setBookingId }) {
           </div> */}
           <CountrySelector></CountrySelector>
           <div className={styles.paymentSection}>
-            <legend className={styles.bookingSubtitle}>
-              Credit Card Details
-            </legend>
-            <p className={styles.hint}>
-              Credit card details doesn't have to match the billing info
-            </p>
+            <legend className={styles.bookingSubtitle}>Credit Card Details</legend>
+            <p className={styles.hint}>Credit card details doesn't have to match the billing info</p>
             <div className={styles.inputBox}>
               <label htmlFor="cardholdername">Cardholders Name</label>
-              <input
-                className={styles.inputField}
-                name="cardholdername"
-                id="cardholdername"
-                type="text"
-                placeholder="Jane Doe"
-                pattern="[A-Za-zæøåÆØÅ]{2,}\s?)+"
-                autoCapitalize="on"
-                autoCorrect="off"
-                title="Please fill out cardholders full name"
-                required
-              />
+              <input className={styles.inputField} name="cardholdername" id="cardholdername" type="text" placeholder="Jane Doe" pattern="[A-Za-zæøåÆØÅ]{2,}\s?)+" autoCapitalize="on" autoCorrect="off" title="Please fill out cardholders full name" required />
             </div>
             <div className={styles.inputBox}>
               <label htmlFor="cardnumber">Cardnumber</label>
-              <input
-                className={`${styles.inputField} ${styles.inputCn}`}
-                name="cardnumber"
-                id="cardnumber"
-                placeholder="1122 3344 5566 7788"
-                required
-                maxLength={16}
-                inputmode="numerical"
-                pattern="[0-9]{16}"
-                title="Please fill out cardnumber"
-              />
+              <input className={`${styles.inputField} ${styles.inputCn}`} name="cardnumber" id="cardnumber" placeholder="1122 3344 5566 7788" required maxLength={16} inputmode="numerical" pattern="[0-9]{16}" title="Please fill out cardnumber" />
             </div>
             <div className={styles.smallerField}>
               <div className={styles.inputBox}>
                 <label htmlFor="expodate">Exp. Date</label>
-                <input
-                  className={`${styles.inputField} ${styles.inputExp}`}
-                  name="expodate"
-                  id="expodate"
-                  placeholder="MM/YY"
-                  required
-                  maxLength={5}
-                  inputmode="numerical"
-                  onChange={checkForError}
-                  pattern="[0-1][0-9][/][0-9]{2}"
-                  title="Please fill out exp. date"
-                />
+                <input className={`${styles.inputField} ${styles.inputExp}`} name="expodate" id="expodate" placeholder="MM/YY" required maxLength={5} inputmode="numerical" onChange={checkForError} pattern="[0-1][0-9][/][0-9]{2}" title="Please fill out exp. date" />
               </div>
               <div className={styles.inputBox}>
                 <label htmlFor="cvc">CVC</label>
-                <input
-                  className={`${styles.inputField} ${styles.inputCvc}`}
-                  name="cvc"
-                  id="cvc"
-                  placeholder="CVC"
-                  required
-                  maxLength={3}
-                  pattern="[0-9]{3}"
-                  title="Please fill out CVC"
-                />
+                <input className={`${styles.inputField} ${styles.inputCvc}`} name="cvc" id="cvc" placeholder="CVC" required maxLength={3} pattern="[0-9]{3}" title="Please fill out CVC" />
               </div>
             </div>
           </div>
--- src/app/components/bookingflow/Ticketinfo.jsx
+++ src/app/components/bookingflow/Ticketinfo.jsx
@@ -11,54 +11,18 @@ function TicketForm({ ticketNumber, ticketType }) {
           <fieldset className={styles.infoBox}>
             <div className={styles.infoBox}>
               <label for="fullname">Full name:</label>
-              <p className={styles.hint}>
-                Write you full name including first, middle, and lastname
-              </p>
-              <input
-                id="fullname"
-                type="text"
-                name="fullname"
-                required
-                className={styles.inputField}
-                placeholder="Jane Doe"
-                pattern="[A-Za-zæøåÆØÅ]{2,}\s?)*"
-                autoCapitalize="on"
-                autoCorrect="off"
-              />
+              <p className={styles.hint}>Write your full name including first, middle, and lastname</p>
+              <input id="fullname" type="text" name="fullname" required className={styles.inputField} placeholder="Jane Doe" pattern="[A-Za-zæøåÆØÅ]{2,}\s?)*" autoCapitalize="on" autoCorrect="off" />
             </div>
             <div className={styles.infoBox}>
               <label for="email">Email:</label>
               <p className={styles.hint}>Email must contain @</p>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                required
-                className={styles.inputField}
-                placeholder="jane@mail.com"
-                pattern="[^@\s]+@[^@\s]+\.[^@\s]+"
-                title="Invalid email address"
-                autoCapitalize="off"
-                autoCorrect="off"
-              />
+              <input id="email" type="email" name="email" required className={styles.inputField} placeholder="jane@mail.com" pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Invalid email address" autoCapitalize="off" autoCorrect="off" />
             </div>
             <div className={styles.infoBox}>
               <label for="phone">Phone-number:</label>
-              <p className={styles.hint}>
-                Phone number must be 8 characters end exclude country-code
-              </p>
-              <input
-                id="phone"
-                type="tel"
-                name="phone"
-                required
-                inputMode="numeric"
-                className={`${styles.inputField} ${styles.inputTel}`}
-                placeholder="20202020"
-                title="Invalid phone number"
-                pattern="^(?:\d{2}\s?\d{2}\s?\d{2}\s?\d{2}|\d{8})$"
-                maxLength={8}
-              />
+              <p className={styles.hint}>Phone number must be 8 characters end exclude country-code</p>
+              <input id="phone" type="tel" name="phone" required inputMode="numeric" className={`${styles.inputField} ${styles.inputTel}`} placeholder="20202020" title="Invalid phone number" pattern="^(?:\d{2}\s?\d{2}\s?\d{2}\s?\d{2}|\d{8})$" maxLength={8} />
             </div>
           </fieldset>
         </div>
